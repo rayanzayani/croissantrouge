2437aee3a31f8508123511e8b0f3f7f80c3fbdc4
rayen <zayanirayan@gmail.com>
2016-11-14 12:09:18 -0800

    modification

--- skin/default/views/about_index.tpl.php
+++ skin/default/views/about_index.tpl.php
@@ -65,7 +65,25 @@
 	  	     	 <h3>A propos croissant rouge</h3>
 	  	     </div>
 	  	    <div class="about-grid">
-				
+				<p>Le Mouvement international de la Croix-Rouge et du Croissant-Rouge est le plus grand réseau humanitaire du monde. Neutre et impartial, il assure protection et assistance à des communautés affectées par des catastrophes et des conflits.
+
+				Le Mouvement rassemble près de 13 millions de volontaires, membres et employés dans 188 pays. Il est constitué de trois composantes principales :
+				•	Le Comité international de la Croix-Rouge (CICR)
+				•	La Fédération internationale des Sociétés de la Croix-Rouge et du Croissant-Rouge (FICR)
+				•	Les Sociétés nationales de la Croix-Rouge et du Croissant-Rouge
+
+				<p>En étroit partenariat, les différents membres du Mouvement aident les communautés vulnérables à devenir plus fortes et plus sûres grâce à des projets de développement et de programmes humanitaires très variés. Le Mouvement s'emploie aussi en coopération avec des gouvernements, des bailleurs de fonds et d'autres organismes d'assistance à aider des personnes vulnérables à travers le monde entier.</p>
+				L’histoire du Mouvement a commencé sur le champ de bataille de Solferino le 24 juin 1859. Henry Dunant, un homme d’affaires genevois de 31 ans, voyageait pour rencontrer l’Empereur français Napoléon III, lequel combattait l’armée autrichienne.
+
+				Bouleversé par un carnage dans lequel 40 000 soldats perdirent la vie ou, blessés, furent abandonnés à leur sort, Dunant décida d’improviser des secours avec les femmes des villages voisins. De retour à Genève quelques jours plus tard, il resta hanté par ce qu’il avait vu sur le champ de bataille. En 1862, il publia Un Souvenir de Solferino, ouvrage dans lequel il relatait ce qu’il avait vu et présentait deux propositions pour améliorer l’assistance aux victimes de la guerre :
+
+				-	Créer, en temps de paix et dans chaque pays, des groupes de volontaires chargés de s'occuper des victimes en temps de guerre ;
+				-	obtenir des pays qu’ils acceptent de protéger les secouristes volontaires et les blessés sur le champ de bataille.
+
+				Le livre fut un immense succès et Dunant voyagea à travers l’Europe pour chercher des soutiens à ses propositions. Le 17 février 1863, avec le soutien de quatre citoyens genevois (un militaire, le Général Guillaume Henry Dufour ; un juriste, Gustave Maunier et deux médecins, Louis Appia et Théodore Maunoir), il fonda le Comité international de secours aux militaires blessés (CISMB), qui devint en 1875 le Comité International de la Croix-Rouge (CICR).</p>
+				Ensemble, toutes les composantes du Mouvement international de la Croix-Rouge et du Croissant-Rouge sont guidées par sept Principes fondamentaux:
+
+				HUMANITE, IMPARTIALITE, NEUTRALITE, INDEPENDANCE, VOLONTARIAT, UNITE ET UNIVERSALITE
 			</div>
 			<div class="clearfix"> </div>
 		</div>
